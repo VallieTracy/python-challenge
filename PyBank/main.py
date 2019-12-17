768d7744954c46b057340a46ef95969d516af0f1
Vallie Tracy <55700939+VallieTracy@users.noreply.github.com>
2019-12-17 15:47:50 -0600

    Working with student_cereal_cleaner exercise

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,7 +4,33 @@ print("-------------------------------------------------")
 import os
 import csv
 
-PyBank_csv = os.path.join('..', 'Resources', 'budget_data.csv')
+# Path to collect data from the Resources folder
+banking_csv = os.path.join('..', 'Resources', 'budget_data.csv')
+
+with open(banking_csv, newline="") as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+
+    for row in csv_reader:
+        if float(row[1]) >= 1000000:
+            print(row)
+    
+
+   
+
+
+
+
+# Read in the CSV file
+#with open(banking_csv, "r") as csvfile:
+
+    # Split the data on commas
+    #csvreader = csv.reader(csvfile, delimiter=',')
+
+    #header = next(csvreader)
+
+    #for row in csvreader:
+        #if float(row[1]) >= 10:
+            #print(row)
 
 
 
