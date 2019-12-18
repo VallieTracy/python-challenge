fc274d9f87e0eb01e38345790d23b1b80b4d89b8
Vallie Tracy <55700939+VallieTracy@users.noreply.github.com>
2019-12-18 09:45:47 -0600

    Changes after working with Python Day 2 Ins_ReadCSV

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,14 +5,30 @@ import os
 import csv
 
 # Path to collect data from the Resources folder
-banking_csv = os.path.join('..', 'Resources', 'budget_data.csv')
+csvpath = os.path.join('Resources', 'budget_data.csv')
 
-with open(banking_csv, newline="") as csvfile:
+with open(csvpath, newline="") as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
 
+    #print(csv_reader)
+
+    csv_header = next(csv_reader)
+    #print(f"CSV Header: {csv_header}")
+
     for row in csv_reader:
-        if float(row[1]) >= 1000000:
-            print(row)
+       print(row[1])
+
+    #for row in csv_reader:
+        #if float(row[1]) >= 1000000:
+            #print(row)
+
+def Numbers(number):
+    print("Total Months: ")
+    
+    for numberValue in number:
+        print(numberValue)
+myList = ["Total Months", "Total", "Average Change", "Greatest Increase in Profits", "Greatest Decrease in Profits"]
+Numbers(myList)
     
 
    
