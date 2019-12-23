daea5ca9d65b30bf13c271c0dfba0dd83b121792
Vallie Tracy <55700939+VallieTracy@users.noreply.github.com>
2019-12-23 12:25:36 -0600

    Rethinking things...

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,39 +5,14 @@ import csv
 # Path to collect data from the Resources folder
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
-#Variables to keep track of various data
-total_months = 0
-prev_revenue = 0
-month_of_change = []
-revenue_change_list = []
+
 
 
 with open(csvpath, newline="") as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
 
-    #print(csv_reader)
-
     csv_header = next(csv_reader)
-    #print(f"CSV Header: {csv_header}")
-
-    for row in csv_reader:
-       
-       total_months = total_months + 1
-
-       revenue_change = int(row["Profit/Losses"]) - prev_revenue
-       prev_revenue = int(row["Profit/Losses"])
-       revenue_change_list = revenue_change_list + [revenue_change]
-       month_of_change = month_of_change + [row["Date"]]
-       
-revenue_avg = sum(revenue_change_list) / len(revenue_change_list)
-
-output = (
-    f"\nFinancial Analysis\n"
-    f"-------------------------\n"
-    f"Total Months: {total_months}\n"
-    f"Average Revenue Change: ${revenue_avg}\n")
-
-print(output)
+    
 
     #for row in csv_reader:
         #if float(row[1]) >= 1000000:
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,16 +3,14 @@ import csv
 
 csvfile = os.path.join("Resources", "election_data.csv")
 
+
+
 with open(csvfile, newline="") as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
 
     csv_header = next(csv_reader)
 
-output = (
-    f"Election Results\n"
-    f"--------------------\n"
-    f"Total Votes\n"
-    f"--------------------\n")
+    
 
-print(output)
+
 
