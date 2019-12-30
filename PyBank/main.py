19e04f50be66a8caa23587b30cfba65ed5e82c86
Vallie Tracy <55700939+VallieTracy@users.noreply.github.com>
2019-12-30 08:41:11 -0600

    Final pushes to PyBank plus minor changes to PyPoll

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,69 +1,24 @@
-# Modules
+#import dependencies
 import os
 import csv
 
-# Set path for file
-csvpath = os.path.join("Resources" , "budget_data.csv")
+#path to csv file
+csvpath = os.path.join("Resources", "budget_data.csv")
 
-# Create lists to store data
-month = []
-profit_loss_table = []
-NewPL = []
-OldPL = []
-amount_change = []
-month_adjusted = []
+#create lists in order to store the data
+individual_periods = []
 
-# Open the CSV
+#open the csv file
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     next(csvreader)
 
-    # Loop through records
+    #loop through the rows in the csv file
     for row in csvreader:
-       month.append(row[0])
-       profit_loss_table.append(int(row[1]))
-    
-    total_amount = sum(profit_loss_table)
-    NewPL = profit_loss_table[1:]
-    OldPL = profit_loss_table[:-1]
-
-    # Option 1 to find Average Change (newest value - oldest value) / (# of months -1)
-    #print("----------------------------")
-    #print(profit_loss_table[0])
-    #print(profit_loss_table[-1])
-    #print("----------------------------")
-    
-    # Option 2 to find Average Change using lists:
-    amount_change = [new - old for (new, old) in zip(NewPL, OldPL)]
-    #amount_change_test = profit_loss_table[]
-    avg_change = round((sum(amount_change) / len(amount_change)),2)
-    
-## Greatest Profit and Loss Values
-    # adjust month list to fit the amount_change list. It takes out the first value 
-    # since the first month does not have a change.
-    month_adjusted = month[1:]
-
-    # find the index number of the highest profit and loss amount in amount_change list
-    MaxIndex = amount_change.index(max(amount_change))
-    MinIndex = amount_change.index(min(amount_change))
-
-    MaxProfitMonth = month_adjusted[MaxIndex]
-    MinProfitMonth = month_adjusted[MinIndex]
-    MaxProfit = amount_change[MaxIndex]
-    MinProfit = amount_change[MinIndex]
-
-# print final hw
-    print("----------------------------")
-    print("Finacial Analysis")
-    print("----------------------------")
-    print("Total Months: " + str(len(month)))
-    print("Total: " + "$" + format(total_amount, ","))
-    print("Average Change: " + "$" + format(avg_change,',.2f'))
-    print("Greatest Increase in Profits: " + MaxProfitMonth + " ($" + format(MaxProfit,',.2f') + ")")
-    print("Greatest Decrease in Profits: " + MinProfitMonth + " ($" + format(MinProfit, ',.2f') + ")")
-    print("----------------------------")
+        individual_periods.append(row[0])
 
 
-
-
-    
+#print output
+print("Financial Analysis")
+print("-----------------------")
+print("Total Months: " + str(len(individual_periods)))
