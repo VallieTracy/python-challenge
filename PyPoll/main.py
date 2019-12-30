92110f378bc8288ed88cb6f78bd77a3c7f285ac1
Vallie Tracy <55700939+VallieTracy@users.noreply.github.com>
2019-12-30 14:51:13 -0600

    starter changes to PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,56 +1,12 @@
-# Import modules
+#Iimport dependencies
 import os
 import csv
 
-# Set path for file
-csvpath = os.path.join("Resources" , "election_data.csv")
+#set path to csv
+csvpath = os.path.join("Resources", "election_data.csv")
 
-# Create lists to store data
-voter_id = []
-county = []
-candidate = []
 
-# Open the CSV
-with open(csvpath, newline="") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
-    next(csvreader)
-
-    for row in csvreader:
-        voter_id.append(row[0])
-        county.append(row[1])
-        candidate.append(row[2])
-    
-    # Count total number of votes cast
-    totalvoters = len(voter_id)
-
-    # unique list of candidates who recieved votes
-    unique_candidates = list(dict.fromkeys(candidate))
-
-    KhanCount = candidate.count("Khan")
-    KhanPercent = KhanCount / totalvoters
-
-    CorreyCount = candidate.count("Correy")
-    CorreyPercent = CorreyCount / totalvoters
-
-    LiCount = candidate.count("Li")
-    LiPercent = LiCount / totalvoters
-
-    OTooleyCount = candidate.count("O'Tooley")
-    OTooleyPercent = OTooleyCount / totalvoters
-
-    def winner(list):
-        return max(set(list), key = list.count)
-
-    print("Election Results")
-    print("----------------------------")
-    print("Total Votes: " + format(totalvoters, ","))
-    print("----------------------------")
-    print("Khan: " + format((KhanPercent*100), ".0f") + "% (" + format(KhanCount, ",") + ")" )
-    print("Correy: " + format((CorreyPercent*100), ".0f") + "% (" + format(CorreyCount, ",") + ")" )
-    print("Li: " + format((LiPercent*100), ".0f") + "% (" + format(LiCount, ",") + ")" )
-    print("O'Tooley: " + format((OTooleyPercent*100), ".0f") + "% (" + format(OTooleyCount, ",") + ")" )
-    print("----------------------------")
-    print("Winner: " + winner(candidate))
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
     
 
 
