5d4298925586dbf82e2dd827423b15173d64ee76
Vallie Tracy <55700939+VallieTracy@users.noreply.github.com>
2019-12-30 15:55:48 -0600

    Unique candidates

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,6 +4,7 @@ import csv
 csvpath = os.path.join("Resources", "election_data.csv")
 
 voter_id = []
+candidate = []
 
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -11,16 +12,20 @@ with open(csvpath, newline="") as csvfile:
 
     for row in csvreader:
         voter_id.append(row[0])
+        candidate.append(row[2])
 
     total_votes = len(voter_id)
     print(total_votes)
+
+    unique_candidate = list(dict.fromkeys(candidate))
+    print(unique_candidate)
 
 
 print("Election Results")
 print("-----------------------")
 print("Total Votes: " + format(total_votes, ","))    
 print("-----------------------")
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+
     
 
 
